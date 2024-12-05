e1920b1387101e828b06d5a2fb153162ad22295e
MontagueM <montaguemoran@improbable.io>
2024-12-05 23:55:42 +0000

    works

--- deploy.ts
+++ deploy.ts
@@ -1,13 +1,29 @@
 import hre from "hardhat";
+import keytar from 'keytar';
+import {ethers} from "ethers";
+
+
+async function getPrivateKey() {
+    const password = await keytar.getPassword(process.env.KEYCHAIN_SERVICE || 'cpk', process.env.KEYCHAIN_ACCOUNT || 'mont');
+    return password;
+}
 
 async function main() {
+    const privateKey = await getPrivateKey();
+    if (!privateKey) {
+        throw new Error("Private key not found in Keychain");
+    }
+
+    const provider = new ethers.JsonRpcProvider("https://dream-rpc.somnia.network/"); // Replace with Somnia RPC URI
+    const wallet = new ethers.Wallet(privateKey, provider);
+
     // We get the contract to deploy
-    const MyContract = await hre.ethers.getContractFactory("MyContract");
-    const myContract = await MyContract.deploy(/* constructor arguments */);
+    const MyContract = await hre.ethers.getContractFactory("carve", wallet);
+    const myContract = await MyContract.deploy();
 
-    await myContract.deployed();
+    await myContract.waitForDeployment();
 
-    console.log("MyContract deployed to:", myContract.address);
+    console.log("MyContract deployed to:", await myContract.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
